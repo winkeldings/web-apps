c011c20c684b1c7299d234e15d2d9acd14f2f57e
winkeldings <88437128+winkeldings@users.noreply.github.com>
2026-09-02 14:34:58 +0000

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/codespaces-react/",
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+});
